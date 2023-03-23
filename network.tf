61b90ad583e02e0faa0ea15e7d07cfcfc0f1aa6f
Valentin Bouiller <31282661+vbouiller@users.noreply.github.com>
2023-03-23 18:02:31 +0000

    Fix: right resource group referenced from main.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/vbouiller-org/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "testGaurav"
+  resource_group_name = "myresourcegroup"
 }
